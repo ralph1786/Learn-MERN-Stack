3866edeb70518d554ac86dab0edbeab0cb9c7de0
ralph1786 <ralph1786@gmail.com>
2019-06-14 17:31:49 -0400

    added auth to update and delete only by owner

--- server/routes/api/remindersRoutes.js
+++ server/routes/api/remindersRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../../middleware/authmiddleware.js");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 //Reminder Model
 const Reminder = require("../../Models/Reminder");
@@ -10,14 +12,19 @@ const Reminder = require("../../Models/Reminder");
 router.get("/", (req, res) => {
   Reminder.find()
     .sort({ date: -1 })
-    .then(reminders => res.json(reminders));
+    .then(reminders => {
+      res.json(reminders);
+    });
 });
 
 //POST api/reminder
 //Create reminder
 router.post("/", authMiddleware, (req, res) => {
+  const token = req.header("x-auth-token");
+  const decoded = jwt.verify(token, config.get("jwtSecret"));
   const newReminder = new Reminder({
-    title: req.body.title
+    title: req.body.title,
+    author: decoded.id
   });
 
   newReminder
@@ -29,26 +36,40 @@ router.post("/", authMiddleware, (req, res) => {
 //PUT api/reminder/:id
 //Update reminder
 router.put("/:id", authMiddleware, (req, res) => {
-  Reminder.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-    (err, reminder) => {
-      if (err) {
-        return res.status(500).json({ message: "Action cannot be processed" });
+  const token = req.header("x-auth-token");
+  const decoded = jwt.verify(token, config.get("jwtSecret"));
+  console.log(decoded);
+  console.log(req.body);
+  // console.log("from body", req.user);
+  if (req.body.author == decoded.id) {
+    Reminder.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+      (err, reminder) => {
+        return res.status(200).json(reminder);
       }
-      return res.status(200).json(reminder);
-    }
-  );
+    );
+  } else {
+    return res
+      .status(404)
+      .json({ message: "Not Allowed To Perform Operation" });
+  }
 });
 
 //DELETE api/reminder/:id
 //Delete reminder
 router.delete("/:id", authMiddleware, (req, res) => {
+  const token = req.header("x-auth-token");
+  const decoded = jwt.verify(token, config.get("jwtSecret"));
   Reminder.findById(req.params.id)
-    .then(reminder =>
-      reminder.remove().then(() => res.json({ message: "Reminder deleted" }))
-    )
+    .then(reminder => {
+      if (reminder.author == decoded.id) {
+        reminder.remove().then(() => res.json({ message: "Reminder deleted" }));
+      } else {
+        res.status(404).json({ message: "Not Allowed To Delete" });
+      }
+    })
     .catch(err => res.status(404).json({ message: "Reminder not deleted" }));
 });
 
